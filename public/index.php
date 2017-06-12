3c4707be3887cb526838f76f543405266ac31868
Robin Schmid <robin@seerow.ch>
2017-06-12 23:08:28 +0200

    yolo

--- public/index.php
+++ public/index.php
@@ -47,7 +47,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-header('Access-Control-Allow-Origin: http://app.klappater.uahnn.com');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization');
 header('Access-Control-Allow-Credentials: true');
